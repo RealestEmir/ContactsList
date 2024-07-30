fad2e84e3c49456f8bd21f62fa266a0f2f25f1c3
RealestEmir <emirabdullah17@outlook.com>
2024-07-30 02:51:40 +0100

    Entry addition improveemnts Made it so the link and image embed works Added remove all button to remove all list entries Tried implementing individual remove product buttons but currently not working

--- js/products.js
+++ js/products.js
@@ -10,36 +10,58 @@ function addProduct(){
         alert("This field is required");
     }
     else{
-        let li = document.createElement("li");
-        li.innerHTML = "";
-        wishList.appendChild(li);
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const imageSrc = e.target.result;
 
-        let a = document.createElement("a");
-        a.innerHTML = productLink.value;
-        li.appendChild(a);
+            let li = document.createElement("li");
+            li.innerHTML = "";
 
-        let p1 = document.createElement("p");
-        p1.innerHTML = productName.value;
-        li.appendChild(p1);
+            let a = document.createElement("a");
+            a.href = productLink.value;
+            li.appendChild(a);
 
-        let p2 = document.createElement("p");
-        p2.innerHTML = productPrice.value;
-        li.appendChild(p2);
+            let p1 = document.createElement("p");
+            p1.innerHTML = productName.value;
+            li.appendChild(p1);
 
-        let img = document.createElement("img");
-        img.innerHTML = productImage.value;
-        a.appendChild(img);
+            let p2 = document.createElement("p");
+            p2.innerHTML = productPrice.value;
+            li.appendChild(p2);
 
-        let p3 = document.createElement("p");
-        p3.innerHTML = productDescription;
-        li.appendChild(p3);
+            let img = document.createElement("img");
+            img.src = imageSrc;
+            a.appendChild(img);
+
+            let p3 = document.createElement("p");
+            p3.innerHTML = productDescription;
+            li.appendChild(p3);
+
+            let button = document.createElement("button");
+            button.innerHTML = "Remove product";
+            button.onclick =  function removeProduct(){
+                li.remove();
+                saveData();
+            };
+            li.appendChild(button);
+
+            wishList.appendChild(li);
+
+            saveData();
+
+            productLink.value = "";
+            productName.value = "";
+            productPrice.value = "";
+            productImage.value = "";
+            productDescription.value = "";
+
+        };
+        reader.readAsDataURL(productImage.files[0]);   
     }
+}
 
-    productLink.value = "";
-    productName.value = "";
-    productPrice.value = "";
-    productImage.value = "";
-    productDescription.value = "";
+function removeAll(){
+    wishList.innerHTML = "";
     saveData();
 }
 
